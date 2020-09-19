ab0e5b7ea3a3dfd6660eb496cf73543f58b3eb08
vitorcoelho <vitorlimac2@gmail.com>
2020-09-19 20:10:30 -0300

    Criando script com comandos basicos do pandas

--- pandas_basico/pandasBasico1.py
+++ pandas_basico/pandasBasico1.py
@@ -2,7 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+## um dataframe pode ser um dict de series
+d = {'coluna1': pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
+     'coluna2': pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd']),
+     'coluna3': pd.Series([10., 2., 8.], index=['b', 'c', 'd'])}
+
 ## criando dataframe
+df = pd.DataFrame(d)
+print(df)
+
+print("Imprimindo coluna 2:")
+print(df['coluna2'])
 
 ## lendo arquivo excel
 
