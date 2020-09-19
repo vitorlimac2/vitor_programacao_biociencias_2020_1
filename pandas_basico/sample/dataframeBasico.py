608b68646662d3927583ee75a73470c563bc3b13
vitor <vitorlimac2@gmail.com>
2020-09-19 20:12:14 -0300

    criando dataframe

---
+++ pandas_basico/sample/dataframeBasico.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+## criando dataframe
+
+
+
+## lendo arquivo excel
+
+## lendo arquivo csv
+
+## escrevendo arquivo excel
+
+## escrevendo arrquivo csv
+
+## atributos do dataframe
+
+
+## adicionando colunas no dataframe
+
+## renomeando colunas do dataframe
+
+## estatisticas basicas
+
+## ordenanacao
+
+## usando operacoes logicas e relacionais para extrair linhas de um dataframe
+
+## outras operacoes com dataframes
+
+## plotando dados
