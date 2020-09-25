db597508af3290bc81036dd4514c5d71f796cf1f
vitor <vitorlimac2@gmail.com>
2020-09-25 15:39:45 -0300

    lista de exercicios 1

---
+++ introducao_prog/ListaExercicio1/ListaExercicio1_questao3.py
@@ -0,0 +1,10 @@
+#### ler o primeiro numero
+
+numero1 = int(input("Digite o primeiro número:"))
+
+### ler o segundo numero
+
+numero2 = int(input("Digite o segundo número:"))
+
+print(numero1 + numero2)
+
