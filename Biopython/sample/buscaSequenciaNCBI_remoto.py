17af51bda00a70a832dc69cc110520d64fd45257
vitor <vitorlimac2@gmail.com>
2020-09-30 19:43:52 -0300

    Arquivos de exemplo para introdução ao biopython

--- Biopython/sample/buscaSequenciaNCBI_remoto.py
+++ Biopython/sample/buscaSequenciaNCBI_remoto.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 
 minhaEntradaFasta = SeqIO.read("/home/vitor/PycharmProjects/VitorCoelho/Biopython/data/TcCLB.506717.80_AA.fasta", format="fasta")
 
-print("Iniciando busca no NCBI..." )
+print("Iniciando busca remota no NCBI..." )
 
 resultadoBuscaBlast = NCBIWWW.qblast("blastp", "nr", minhaEntradaFasta.seq, format_type="Text",expect=0.05, hitlist_size=10)
 
