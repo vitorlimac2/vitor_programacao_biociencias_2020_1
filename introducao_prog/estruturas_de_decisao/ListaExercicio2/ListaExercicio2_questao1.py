33692fc84935aab96878d5a08ad0835e3c32ee47
vitorcoelho <vitorlimac2@gmail.com>
2020-10-03 21:09:03 -0300

    Lista 2 - questao 1

---
+++ introducao_prog/estruturas_de_decisao/ListaExercicio2/ListaExercicio2_questao1.py
@@ -0,0 +1,13 @@
+## Lista de Exercicio 2 - questao 1
+## ler um número inteiro
+
+num = int(input("Digite um número:"))
+
+## informar se o número é par ou ímpar.
+
+if num % 2 == 0:
+    print("O número %d é par" % num)
+else:
+    print("O número %d é ímpar" % num)
+
+
